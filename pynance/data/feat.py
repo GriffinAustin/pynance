9e2c1ca31598b2d7762768749c744648bf255f26
Marshall Farrier <info@marshallfarrier.com>
2015-02-17 15:41:15 -0800

    Eliminate specialized functions; indirect tests for data.feat.fromfuncs

--- pynance/data/feat.py
+++ pynance/data/feat.py
@@ -5,76 +5,20 @@ Copyright (c) 2014 Marshall Farrier
 license http://opensource.org/licenses/MIT
 
 These functions are intended to be used in conjunction
-with `functools.partial` to pass to `data.labeledfeatures()`.
+with `functools.partial` and other function decorators
+to pass to `data.labeledfeatures()`.
 For example,
 
 >>> from functools import partial
->>> features, labels = pn.data.labeledfeatures(eqdata, 
-        partial(pn.data.feat.growth_vol(256, 252, 'Adj Close'), labelfunc)
+>>> featfunc = pn.decorate(partial(pn.data.feat.fromfuncs, [fn1, fn2, fn3], skipatstart=averaging_window), 
+        averaging_window + n_feature_sessions - 1)
+>>> features, labels = pn.data.labeledfeatures(eqdata, featfunc, labelfunc) 
 """
 
-from __future__ import absolute_import
 from functools import partial
 
 import numpy as np
 import pandas as pd
-
-from .. import common
-from .. import tech
-
-def growth_vol(n_sessions, eqdata, **kwargs):
-    """
-    Derive growth and relative volume data from `eqdata` and
-    combine into a set of features.
-
-    Using partial to fix `n_sessions` or keyword arguments,
-    this function can be passed as `featurefunc` to `data.labeledfeatures()`.
-
-    Parameters
-    --
-    n_sessions : int
-        Number of sessions over which price and volume will be
-        used as features.
-
-    eqdata : DataFrame
-        Data from which to build features.
-
-    averaging_interval : int, optional
-        Interval over which to average volume. Defaults to 252 (1 year).
-
-    pricecol : str, optional
-        Column to use for prices. Defaults to 'Adj Close'
-
-    skipatend : int, optional
-        Rows to skip at the end. Used for synchronizing the indices of
-        labels and features. Defaults to 0.
-
-    constfeat : bool, optional
-        Determines whether or not the constant 1. will appear
-        as first feature in each row. Defaults to `True`.
-
-    Returns
-    --
-    features : DataFrame
-        The feature set determined by the input parameters.
-
-    skipatstart : int
-        The row number of the first feature
-        as measured within the index of `eqdata`. This is 
-        used when the function is passed to `labeledfeatures()`
-        for synchronizing labels with features.
-    """
-    _averaging_interval = kwargs.get('averaging_interval', 252)
-    _pricecol = kwargs.get('pricecol', 'Adj Close')
-    _skipatend = kwargs.get('skipatend', 0)
-    _skipatstart = n_sessions + _averaging_interval - 1
-    _growth = tech.growth(eqdata, selection=_pricecol, skipstartrows=(_averaging_interval - 1),
-        skipendrows=_skipatend)
-    _vol = tech.ratio_to_ave(_averaging_interval, eqdata, skipendrows=_skipatend, outputcol='Rel Vol')
-    _features = _featurize_growth_vol(_growth, _vol, n_sessions) 
-    if kwargs.get('constfeat', True):
-        _features = add_const(_features)
-    return _features, _skipatstart
 
 def add_const(features):
     """
@@ -93,20 +37,6 @@ def add_const(features):
     content[:, 1:] = features.iloc[:, :].values
     cols = ['Constant'] + features.columns.tolist()
     return pd.DataFrame(data=content, index=features.index, columns=cols, dtype='float64')
-
-def _featurize_growth_vol(growth, volume, n_sessions):
-    """
-    Combine growth and volume data into a set of features
-    """
-    growth_feat = common.featurize(growth, n_sessions, selection='Growth')
-    vol_feat = common.featurize(volume, n_sessions, selection='Rel Vol')
-    growth_cols = map(partial(_concat, strval='G'), range(-n_sessions + 1, 1))
-    vol_cols = map(partial(_concat, strval='V'), range(-n_sessions + 1, 1))
-    all_cols = list(growth_cols) + list(vol_cols)
-    features = pd.DataFrame(index=growth_feat.index, columns=all_cols, dtype='float64')
-    features.iloc[:, :n_sessions] = growth_feat.values
-    features.iloc[:, n_sessions:] = vol_feat.values
-    return features
 
 def fromcols(selection, n_sessions, eqdata, **kwargs):
     """
@@ -209,5 +139,4 @@ def fromfuncs(funcs, n_sessions, eqdata, **kwargs):
     return _features
 
 def _concat(intval, strval):
-    """ helper for creating columns in `_featurize_growth_vol()` """
     return str(intval) + strval
--- tst/data/test_combine.py
+++ tst/data/test_combine.py
@@ -38,19 +38,24 @@ class TestData(unittest.TestCase):
         _n_featsess = 2
         _ave_int = 3
         _pred_int = 1
-        _featfunc = partial(pn.data.feat.growth_vol, _n_featsess, averaging_interval=_ave_int)
+        _featfuncs = []
+        # growth and relative volume functions
+        _featfuncs.append(pn.decorate(partial(pn.tech.growth, selection='Adj Close'), title='G'))
+        _featfuncs.append(pn.decorate(partial(pn.tech.ratio_to_ave, _ave_int), title='V'))
+        _featfunc = pn.decorate(partial(pn.data.feat.fromfuncs, _featfuncs, _n_featsess, skipatstart=_ave_int),
+                _ave_int + _n_featsess - 1)
         _labfunc = pn.decorate(partial(pn.data.lab.growth, _pred_int, 'Adj Close'), _pred_int)
         features, labels = pn.data.labeledfeatures(self.equity_data, _featfunc, _labfunc)
         self.assertEqual(features.values.shape[0], labels.values.shape[0])
         self.assertEqual(features.values.shape[1], 5)
         for i in range(1, len(features.index)):
-            self.assertAlmostEqual(features.loc[:, '-1G'].values[i], 
-                    features.loc[:, '0G'].values[i - 1])
-            self.assertAlmostEqual(features.loc[:, '-1V'].values[i], 
-                    features.loc[:, '0V'].values[i - 1])
+            self.assertAlmostEqual(features.loc[:, '-1 G'].values[i], 
+                    features.loc[:, '0 G'].values[i - 1])
+            self.assertAlmostEqual(features.loc[:, '-1 V'].values[i], 
+                    features.loc[:, '0 V'].values[i - 1])
         for i in range(5):
-            self.assertAlmostEqual(features.loc[:, '0G'].values[i], (i + 5.) / (i + 4.))
-            self.assertAlmostEqual(features.loc[:, '0V'].values[i], (2. * i + 9.) / (2. * i + 5.))
+            self.assertAlmostEqual(features.loc[:, '0 G'].values[i], (i + 5.) / (i + 4.))
+            self.assertAlmostEqual(features.loc[:, '0 V'].values[i], (2. * i + 9.) / (2. * i + 5.))
             self.assertAlmostEqual(labels.values[i], (i + 6.) / (i + 5.))
 
 if __name__ == '__main__':
