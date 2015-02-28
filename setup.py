2cb424bf1fd879ad86072c965d75e82d457af231
Marshall Farrier <info@marshallfarrier.com>
2015-02-28 04:40:25 -0800

    Setup files to include package on PyPI.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name='pynance',
+        packages=['pynance'],
+        version='0.1',
+        description='Library for retrieving and analysing financial data',
+        author='Marshall Farrier',
+        author_email='marshalldfarrier@gmail.com',
+        url='https://github.com/aisthesis/pynance',
+        download_url='https://github.com/aisthesis/pynance/tarball/0.1',
+        keywords=['finance'],
+        classifiers=[],
+        )
