16f11f3c73a4375f84e3fdee37afccf9c741356e
Marshall Farrier <info@marshallfarrier.com>
2015-02-27 21:43:40 -0800

    Double calendar spread.

--- pynance/opt/spread.py
+++ pynance/opt/spread.py
@@ -44,8 +44,49 @@ def calendar(optdata, opttype, strike, expiry1, expiry2):
     _vals = np.array([_price1, _price2, _price2 - _price1, _underlying])
     return pd.DataFrame(_vals, index=_index, columns=['Value'])
 
-def dbl_calendar(opdata, putstrike, callstrike, expiry1, expiry2):
-    pass
+def dbl_calendar(optdata, lowstrike, highstrike, expiry1, expiry2):
+    """
+    Metrics for evaluating a double calendar spread.
+
+    Parameters
+    --
+    optdata : DataFrame
+        Data returned from `pn.opt.get()`
+
+    opttype : str {'call', 'put'}
+        Type of option on which to collect data.
+
+    lowstrike : numeric
+        Lower strike price. To be used for put spread.
+
+    highstrike : numeric
+        Higher strike price. To be used for call spread.
+
+    expiry1 : date or date str (e.g. '2015-01-01')
+        Earlier expiration date.
+
+    expiry2 : date or date str (e.g. '2015-01-01')
+        Later expiration date.
+
+    Returns
+    --
+    metrics : DataFrame
+        Metrics for evaluating spread.
+    """
+    _index = ['Near Put', 'Far Put', 'Put Ratio', 'Near Call', 'Far Call', 'Call Ratio', 'Debit', 'Underlying']
+    _metrics = pd.DataFrame(index=_index, columns=['Value'])
+    _nearput, _metrics.loc['Underlying', 'Value'] = price.get(optdata, 'put', lowstrike, expiry1, False) 
+    _metrics.loc['Near Put', 'Value'] = _nearput
+    _farput, _ = price.get(optdata, 'put', lowstrike, expiry2, False)
+    _metrics.loc['Far Put', 'Value'] = _farput
+    _metrics.loc['Put Ratio', 'Value'] = _nearput / _farput
+    _nearcall, _  = price.get(optdata, 'call', highstrike, expiry1, False)
+    _metrics.loc['Near Call', 'Value'] = _nearcall
+    _farcall, _ = price.get(optdata, 'call', highstrike, expiry2, False)
+    _metrics.loc['Far Call', 'Value'] = _farcall
+    _metrics.loc['Call Ratio', 'Value'] = _nearcall / _farcall
+    _metrics.loc['Debit', 'Value'] = _farcall + _farput - _nearcall - _nearput
+    return _metrics
 
 def straddle(optdata, strike, expiry):
     """
