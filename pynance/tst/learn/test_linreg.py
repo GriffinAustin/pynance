4cb1d9f6475f039faa58081eb452f1e10c91252d
Marshall Farrier <info@marshallfarrier.com>
2016-01-27 23:03:08 -0800

    Skeleton for linear regression test

---
+++ pynance/tst/learn/test_linreg.py
@@ -0,0 +1,28 @@
+"""
+Copyright (c) 2015 Marshall Farrier
+license http://opensource.org/licenses/MIT
+
+@author: Marshall Farrier
+@contact: marshalldfarrier@gmail.com
+@since: 2014-11-02
+@summary: Unit tests for linear regression
+"""
+
+import sys
+import unittest
+
+import numpy as np
+import pandas as pd
+
+import pynance as pn
+
+class TestData(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_run(self):
+       pass 
+
+if __name__ == '__main__':
+    unittest.main()
